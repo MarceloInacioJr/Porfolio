0a34426672de53a7afd250e98e7eddab139a51fa
Marcelo Inacio <marceloinacio2121@gmail.com>
2024-03-25 22:42:48 -0300

    Adding project content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 
+// projects
+const projects=[
+    {
+        nome:"Google",
+        subname:"Projo google feito com HTML e CSS,feito com as propriedades flex box",
+        url:'../assets/main/icon-projects-video/video/google.mp4'
+    }
+]
+
+
+function loadingProjects(){
+
+    let projectsContainer = document.querySelector('#projects-container')
+
+    projects.forEach(projectObj =>{
+        let project = docuemnt.createElement('div');
+        project.classList.add('project')
+
+        
+    })
+
+}
