28eaf1e7b16c4fafba72a49469b75965afb762b5
Marcelo Inacio <marceloinacio2121@gmail.com>
2024-04-10 23:02:55 -0300

    Adding lload content function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+// loading content main
+
+
+function isVisible (element){
+    let rect = element.getBoundingClientRect()
+    return (
+        rect.top <= window.innerHeight /2 &&
+        rect.bottom > window.innerHeight /2       
+    )    
+}
+
+function loading(element){
+    if (isVisible(element)){
+        element.classList.add('visible-content')
+        console.log('activate')
+    }else{
+        element.classList.remove('visible-content')
+        console.log('desactive')
+    }
+    
+}
+
+window.addEventListener('scroll',()=>{
+    let aboutme = document.querySelector('#about-me')
+    let skills = document.querySelector('#skills')
+    let training = document.querySelector('#training')
+    let projects = document.querySelector('#projects')
+    let contact = document.querySelector('#contact')
+
+    loading(aboutme)
+    loading(skills)
+    loading(training)
+    loading(projects)
+    loading(contact)
+
+})
+
+
+
+
+
+
 
 // projects
 const projects=[
